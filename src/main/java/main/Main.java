300f3c6082eda282b33549b6169dda3da0b8f6c8
calvinmar <calvinmar@localhost>
2026-04-08 08:51:29 -0400

    Something is off with mine, but this is main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+	@Override
+	public void start(Stage stage) throws Exception{
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/adderView.fxml"));
+		
+		Pane view = loader.load();
+		
+		Scene s = new Scene(view);
+		stage.setScene(s);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
