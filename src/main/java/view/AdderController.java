f4de8b6fdcdfde46d806598b739370b4fe5612bc
Jenna <jenna.nicospace@gmail.com>
2026-04-08 08:49:56 -0400

    Controller accesses model

--- src/main/java/view/AdderController.java
+++ src/main/java/view/AdderController.java
@@ -4,8 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.AdderModel;
 
 public class AdderController {
+	
+	AdderModel model;
+	
+	public void setModel(AdderModel model) {
+		this.model = model;
+	}
 	
     @FXML
     private TextField firstNumTextField;
