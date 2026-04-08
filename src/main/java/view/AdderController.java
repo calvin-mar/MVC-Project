9abf858c365436f960a43d4e96a788d99deaa7e2
Jenna <jenna.nicospace@gmail.com>
2026-04-08 08:49:11 -0400

    Controller skeleton and synced

---
+++ src/main/java/view/AdderController.java
@@ -0,0 +1,24 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AdderController {
+	
+    @FXML
+    private TextField firstNumTextField;
+
+    @FXML
+    private Label resultNumLabel;
+
+    @FXML
+    private TextField secondNumTextField;
+
+    @FXML
+    void onAddButton(ActionEvent event) {
+
+    }
+
+}
